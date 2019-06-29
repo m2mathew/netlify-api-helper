933b5a3f2beb33931605d5af0004085c8d6fb564
Mike Mathew <m2mathew@me.com>
2019-06-29 13:58:05 -0500

    Adds netlify svg logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// Internal Dependencies
+import NetlifyLogo from './NetlifyLogo.svg';
+
 // Local Variables
 const HeaderWrapper = styled.header`
-  padding: 4em;
   background: #f2f5f7;
+  padding: 2em;
+  border-bottom: 5px solid #0e1e24;
 `;
 
 // Component Definition
 function Header() {
   return (
     <HeaderWrapper>
-      <h4>[Logo goes here]</h4>
+      <NetlifyLogo />
     </HeaderWrapper>
   );
 }
