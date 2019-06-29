3ab15eae49a7fc874ad987953c0e7b336377ea78
Mike Mathew <m2mathew@me.com>
2019-06-29 10:58:06 -0500

    Tweaks prettier to use singleQuotes. 💪

---
+++ src/components/Wrapper.js
@@ -0,0 +1,15 @@
+// External Dependencies
+// import React from "react";
+import { styled } from 'styled-components';
+
+const world = 'hello';
+
+console.log(world);
+
+// Component Definition
+const Wrapper = styled.section`
+  padding: 4em;
+  background: rebeccapurple;
+`;
+
+export default Wrapper;
